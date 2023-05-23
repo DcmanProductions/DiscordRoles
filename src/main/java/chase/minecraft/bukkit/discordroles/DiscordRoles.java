9890e466b4f0c54a2c676dde89318d105928d280
Drew Chase <dcmanproductions@gmail.com>
2023-05-22 20:27:28 -0400

    added config and commands

--- src/main/java/chase/minecraft/bukkit/discordroles/DiscordRoles.java
+++ src/main/java/chase/minecraft/bukkit/discordroles/DiscordRoles.java
@@ -1,17 +1,43 @@
 package chase.minecraft.bukkit.discordroles;
 
+import chase.minecraft.bukkit.discordroles.bot.DiscordBot;
+import chase.minecraft.bukkit.discordroles.command.BotRolesCommand;
+import chase.minecraft.bukkit.discordroles.command.LinkCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DiscordRoles extends JavaPlugin {
+import java.util.Objects;
+import java.util.logging.Logger;
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
 
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+public final class DiscordRoles extends JavaPlugin
+{
+	
+	
+	public static Logger log = Bukkit.getLogger();
+	
+	public static FileConfiguration config;
+	
+	@Override
+	public void onEnable()
+	{
+		log.info("Starting Plugin");
+		config = getConfig();
+		config.addDefault("guild-id", 0L);
+		config.options().copyDefaults(true);
+		saveDefaultConfig();
+		log.info("ID: "+DiscordRoles.config.getLong("guild-id"));
+		DiscordBot.start();
+		Objects.requireNonNull(getCommand("link")).setExecutor(new LinkCommand());
+		Objects.requireNonNull(getCommand("discordroles")).setExecutor(new BotRolesCommand());
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		DiscordBot.getInstance().stop();
+		log.warning("Stopping Plugin");
+	}
+	
 }
