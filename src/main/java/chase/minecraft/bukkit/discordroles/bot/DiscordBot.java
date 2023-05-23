07737d5af4d059614f6ae5916a55d30a8bf74f27
Drew Chase <dcmanproductions@gmail.com>
2023-05-22 21:53:48 -0400

    removed and reset token and added it to properties file

--- src/main/java/chase/minecraft/bukkit/discordroles/bot/DiscordBot.java
+++ src/main/java/chase/minecraft/bukkit/discordroles/bot/DiscordBot.java
@@ -1,25 +1,23 @@
 package chase.minecraft.bukkit.discordroles.bot;
 
 import chase.minecraft.bukkit.discordroles.DiscordRoles;
-import com.google.gson.Gson;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
-import net.dv8tion.jda.api.utils.FileUpload;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.io.InputStream;
+import java.util.*;
 
 public class DiscordBot
 {
@@ -29,11 +27,39 @@ public class DiscordBot
 	
 	@Nullable
 	private static DiscordBot instance;
+	
 	private final JDA bot;
 	
 	protected DiscordBot()
 	{
-		this.bot = JDABuilder.createDefault("MTEwODE2NTQ1NzM1OTQwNTA5Ng.Gm6HzV.yRMFFSWNxsGDzFxDP-vEDdQmERfOxSt0d3o7JI").build();
+		Properties properties = new Properties();
+		try (InputStream fs = DiscordRoles.class.getResourceAsStream("/bot.properties"))
+		{
+			properties.load(fs);
+		} catch (IOException e)
+		{
+			DiscordRoles.log.severe("Unable to load bot token: " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+		this.bot = JDABuilder.createDefault(properties.getProperty("token")).build();
+		this.bot.addEventListener(new ListenerAdapter()
+		{
+			@Override
+			public void onButtonInteraction(@NotNull ButtonInteractionEvent event)
+			{
+				if (event.getComponentId().equalsIgnoreCase("accept-link-button"))
+				{
+					User user = event.getUser();
+					Guild guild = getGuild();
+					if (guild != null)
+					{
+						guild.addRoleToMember(user, getOrCreateRole());
+					}
+				}
+				super.onButtonInteraction(event);
+			}
+		});
 	}
 	
 	/**
@@ -56,46 +82,29 @@ public class DiscordBot
 				EmbedBuilder embedBuilder = new EmbedBuilder()
 						.setTitle("Link Your Minecraft Account?")
 						.setDescription("Hello %s, would you like to link your minecraft account with %s?".formatted(mc, guild.getName()));
-				Button button = Button.primary("accept-link-button", "Link Account!");
 				
-				File local = getBannerImage();
-				if (local != null) {
-					embedBuilder.setImage("attachment://banner");
-					DiscordRoles.log.info(local.getAbsolutePath());
-				}
+				Button accept = Button.primary("accept-link-button", "Link Account!");
+				Button learnmore = Button.link("https://github.com/dcmanProductions/discordroles", "Learn More...");
 				
-				user.openPrivateChannel().queue(channel -> {
-					if (local != null) {
-						try {
-							FileInputStream fs = new FileInputStream(local);
-							channel.sendFiles(FileUpload.fromData(fs, "banner")).setEmbeds(embedBuilder.build()).setActionRow(button).queue(success -> {
-								try {
-									fs.close();
-								} catch (IOException e) {
-									DiscordRoles.log.severe("Unable to send image: " + e.getMessage());
-									e.printStackTrace();
-								}
-							}, failure -> {
-								try {
-									fs.close();
-								} catch (IOException e) {
-									DiscordRoles.log.severe("Unable to send image: " + e.getMessage());
-									e.printStackTrace();
-								}
-							});
-						} catch (IOException e) {
-							DiscordRoles.log.severe("Unable to send image: " + e.getMessage());
-							e.printStackTrace();
-						}
-					} else {
-						channel.sendMessageEmbeds(embedBuilder.build()).setActionRow(button).queue();
-					}
+				embedBuilder.setImage(DiscordRoles.config.getString("banner-image"));
+				user.openPrivateChannel().queue(channel ->
+				{
+					channel.sendMessageEmbeds(embedBuilder.build()).setActionRow(accept, learnmore).queue();
 				});
 				
 			}
 			return new MessageResponse("User linked", true);
 		}
 		return new MessageResponse("Unable to find user: %s".formatted(discord), false);
+	}
+	
+	public Role getOrCreateRole()
+	{
+		String name = DiscordRoles.config.getString("server-name");
+		Guild guild = getGuild();
+		assert guild != null;
+		assert name != null;
+		return guild.getRolesByName(name, true).stream().findFirst().orElseGet(() -> guild.createRole().setName(name).complete());
 	}
 	
 	public String generateInviteLink()
@@ -147,18 +156,6 @@ public class DiscordBot
 		return bot.getGuildById(guildId);
 	}
 	
-	private static @Nullable File getBannerImage()
-	{
-		String[] types = new String[]{"png", "jpg", "jpeg", "gif", "webp"};
-		for (String type : types)
-		{
-			File local = new File(DiscordRoles.getPlugin(DiscordRoles.class).getDataFolder(), "banner." + type);
-			if (local.exists())
-				return local;
-		}
-		return null;
-	}
-	
 	/**
 	 * Starts the bot with the given token
 	 *
@@ -189,7 +186,7 @@ public class DiscordBot
 	{
 		if (instance == null)
 		{
-			throw new RuntimeException("Discord Bot is not running");
+			start();
 		}
 		return instance;
 	}
